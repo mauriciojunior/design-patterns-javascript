dadf277df206c4c7c021d4e438861ad3e1253b44
Maurício Júnior <mjuniior92@gmail.com>
2016-05-06 23:21:25 -0300

    Update task.js

--- Constructor/task.js
+++ Constructor/task.js
@@ -1,23 +1,23 @@
 var Task = function(name) {
-	this.name = name;
-	this.completed = false;
-
+	this.name = name
+	this.completed = false
+	
 	this.complete = function() {
-		console.log('completing Task: ' + this.name);
-		this.completed = true;
-	};
-
+	console.log('completing Task: ' + this.name);
+	this.completed = true;
+	}
+	
 	this.save = function() {
-		console.log('saving Task: ' + this.name)
+	console.log('saving Task: ' + this.name)
 	}
 }
 
-var task1 = new Task('create a demo for contructors');
-var task2 = new Task('create a demo for modules');
-var task3 = new Task('create a demo for singletons');
-var task4 = new Task('create a demo for prototypes');
+var task1 = new Task('create a demo for contructors')
+var task2 = new Task('create a demo for modules')
+var task3 = new Task('create a demo for singletons')
+var task4 = new Task('create a demo for prototypes')
 
-task1.save();
-task2.complete();
-task3.complete();
-task4.save();
+task1.save()
+task2.complete()
+task3.complete()
+task4.save()
